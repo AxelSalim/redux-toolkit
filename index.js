ffb7b73c4c82ee89bdca9d6985f09eee2ea4bf49
AxelSalim <adjakidjememiaghe@gmail.com>
2024-10-19 14:37:18 +0100

    modification du index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import store from "./app/store.js"; 
+import { cakeActions } from "./features/cake/cakeSlice.js";
 
+console.log("Initial state ", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+    console.log("Updated state ", store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked());
+
+unsubscribe();
