1b0873b048e22c4f07ae9d2ffa613ff0c3af1801
AxelSalim <adjakidjememiaghe@gmail.com>
2024-10-19 14:53:56 +0100

    ajout du icecreamReducer

--- app/store.js
+++ app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cakeReducer from "../features/cake/cakeSlice.js";
+import icecreamReducer from "../features/icecream/icecreamSlice.js"
 
 const store = configureStore ({
     reducer: {
         cake: cakeReducer,
+        icecream: icecreamReducer,
     }
 })
 
