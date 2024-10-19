cc736ad0720d4e8ef00ff8bc043612661e64ba53
AxelSalim <adjakidjememiaghe@gmail.com>
2024-10-19 15:06:31 +0100

    modification du store

--- app/store.js
+++ app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import cakeReducer from "../features/cake/cakeSlice.js";
 import icecreamReducer from "../features/icecream/icecreamSlice.js"
 
-const store = configureStore ({
+const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
